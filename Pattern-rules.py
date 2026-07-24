d845f706f5afaaf5c9a649cb6796467169c9821a
Priyanka <priyankadhot24@gmail.com>
2026-07-24 10:18:45 +0530

    star patterns

---
+++ Pattern-rules.py
@@ -0,0 +1,7 @@
+#----  The key to all patterns is understanding only 3 things:
+
+#----   Rows → How many lines?
+
+#----   Spaces → How many spaces before the pattern?
+
+#----   Stars/Numbers → How many *, numbers, or letters?
